359c35ec632b69fa2b8bff34685288359d930a3d
ghkinkin <46836452+ghkinkin@users.noreply.github.com>
2019-01-19 17:40:02 +0800

    Update setup-linux-pool-edited.sh

--- setup-linux-pool-edited.sh
+++ setup-linux-pool-edited.sh
@@ -3,29 +3,25 @@ echo '# Running apt-get update ...'
 apt-get update
 echo '# Installing imagemagick ...'
 apt-get install -y imagemagick
+echo '# Installing blender ...'
+apt-get install -y blender
 echo '# Installing python-pip ...'
 apt-get install -y python-pip
 echo '# Installing azure-batch ...'
-pip install azure-batch==4.1.3
+pip install azure-batch
 echo '# Installing packages-microsoft-prod.deb ...'
 wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 echo '# Installing apt-transport-https ...'
+sudo add-apt-repository -y universe
 sudo apt-get -y install apt-transport-https
 echo '# Running apt-get update ...'
 sudo apt-get update
-echo '# Installing install dotnet-sdk-2.1 ...'
-sudo apt-get -y install dotnet-sdk-2.1
+echo '# Installing install dotnet-sdk-2.2 ...'
+sudo apt-get -y install dotnet-sdk-2.2
 wget -O azcopy.tar.gz https://aka.ms/downloadazcopyprlinux
 tar -xf azcopy.tar.gz
-
 ./install.sh
-
-echo '# Installing blender ...'
-sudo add-apt-repository -y ppa:thomas-schiex/blender
-sudo apt-get update
-apt-get install -y blender
 blender --version
-
 echo "## DONE ##"
 exit $?
