f06fa5b7f7f8cca8308b4ea96fb955d98077047f
ghkinkin <46836452+ghkinkin@users.noreply.github.com>
2019-01-19 16:44:25 +0800

    Update setup-linux-pool-edited.sh

--- setup-linux-pool-edited.sh
+++ setup-linux-pool-edited.sh
@@ -3,12 +3,14 @@ echo '# Running apt-get update ...'
 apt-get update
 echo '# Installing imagemagick ...'
 apt-get install -y imagemagick
+echo '# Installing blender ...'
+apt-get install -y blender
 echo '# Installing python-pip ...'
 apt-get install -y python-pip
 echo '# Installing azure-batch ...'
 pip install azure-batch==4.1.3
 echo '# Installing packages-microsoft-prod.deb ...'
-wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 echo '# Installing apt-transport-https ...'
 sudo apt-get -y install apt-transport-https
@@ -18,12 +20,6 @@ echo '# Installing install dotnet-sdk-2.1 ...'
 sudo apt-get -y install dotnet-sdk-2.1
 wget -O azcopy.tar.gz https://aka.ms/downloadazcopyprlinux
 tar -xf azcopy.tar.gz
-echo '# Add the Official PPA Provided by Blender Team ...'
-add-apt-repository -y ppa:thomas-schiex/blender
-echo '# Running apt-get update ...'
-apt-get update
-echo '# Installing blender ...'
-apt-get install -y blender
 blender --version
 ./install.sh
 echo "## DONE ##"
