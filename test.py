323f45d44c6d2fc597890d4df9b0ee5bb3cbce4f
ghkinkin <46836452+ghkinkin@users.noreply.github.com>
2019-01-22 19:04:54 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
 import bpy, _cycles
+
+bpy.context.user_preferences.addons['cycles'].preferences.compute_device_type = 'CUDA'
+bpy.context.user_preferences.addons['cycles'].preferences.compute_device = 'CUDA_MULTI_2'
+
 print(_cycles.available_devices())
 bpy.context.user_preferences.system.compute_device = 'BLABLABLA'
