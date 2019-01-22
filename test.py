33533fac1a1d5aad2dd1d9866523499f52355baf
ghkinkin <46836452+ghkinkin@users.noreply.github.com>
2019-01-22 19:12:01 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import bpy, _cycles
 
-bpy.context.user_preferences.addons['cycles'].preferences.compute_device_type = 'CUDA'
-bpy.context.user_preferences.addons['cycles'].preferences.compute_device = 'CUDA_MULTI_2'
+bpy.context.user_preferences.system.compute_device_type = 'CUDA'
+bpy.context.user_preferences.system.compute_device = 'CUDA_MULTI_2'
 
 print(_cycles.available_devices())
+
 bpy.context.user_preferences.system.compute_device = 'BLABLABLA'
 bpy.context.user_preferences.system.compute_device = 'CUDA'
 
