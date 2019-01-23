b19051a1f70328d649999ef471f2f4217fd8a32e
ghkinkin <46836452+ghkinkin@users.noreply.github.com>
2019-01-23 10:27:23 +0800

    Create test10.py

---
+++ test10.py
@@ -0,0 +1,6 @@
+import bpy, _cycles
+
+print(_cycles.available_devices())
+
+bpy.context.user_preferences.addons['cycles'].preferences.compute_device_type = 'CUDA'
+bpy.context.user_preferences.addons['cycles'].preferences.compute_device = 'yafudha'
